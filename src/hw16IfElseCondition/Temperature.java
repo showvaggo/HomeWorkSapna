3b7404c3b67d5935bbacdc25f932215e0bb81bfa
Showvaggo <sr8950601@gmail.com>
2024-09-22 03:00:26 -0400

    update

--- src/hw16IfElseCondition/Temperature.java
+++ src/hw16IfElseCondition/Temperature.java
@@ -2,9 +2,6 @@ package hw16IfElseCondition;
 
 import java.util.Scanner;
 
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane.IconifyAction;
-import javax.swing.text.Utilities;
-
 public class Temperature {
 
 	public static void main(String[] args) {
@@ -13,39 +10,43 @@ public class Temperature {
 	}
 
 	Scanner scanner = new Scanner(System.in);
-	int tem1 = scanner.nextInt();
-	int tem2 = scanner.nextInt();
-	int tem3 = scanner.nextInt();
-	int tem4 = scanner.nextInt();
-	int tem5=scanner.nextInt();
+
+	int todaysTemperature1 = scanner.nextInt();
+	int todaysTemperature2 = scanner.nextInt();
+	int todaysTemperature3 = scanner.nextInt();
+	int todaysTemperature4 = scanner.nextInt();
+	int todaysTemperature5 = scanner.nextInt();
 	{
-		if (tem1 <32, tem2<55, tem3<73, tem4<101, >tem5 ) {
-			System.out.println("");
-			
+		if (todaysTemperature1 < 32) {
+			System.out.println(".....Freezing......");
+
+		} else if (todaysTemperature2 < 55) {
+			System.out.println("....Pleasent......");
+
+		} else if (todaysTemperature3 < 73) {
+			System.out.println("......Getting Warmer......");
+
+		} else if (todaysTemperature4 < 101) {
+			System.out.println(".....Hot.....");
+
+		} else if (todaysTemperature5 > 101) {
+			System.out.println("......Very Hot.....");
+
+		} else {
+			System.out.println("....Please put a valid temperature...");
+			scanner.close();
+
 		}
-		
-	}else if(todaysTemperature<tem1)
-	
-		{System.out.println("Freezing");
 
-	}else if(todaysTemperature<tem2)
-	{
-		System.out.println("Pleasent");
-	}else if(todaysTemperature<tem3)
-	{
-		System.out.println("Getting Warmer");
+		{
 
-	}else if(todaysTemperature<tem4)
-	{
-		System.out.println("Hot");
+		}
+		{
 
-	}else if(todaysTemperature>tem5)
-	{
-		System.out.println("Very Hot");
-	}else
-	{
-		System.out.println(".....Please put a valid temperature......");
-		scanner.close();
+		}
+		{
+
+		}
 
 	}
-}}
+}
